aca0b8f51aa0fd75b742744cdfe4d232e2437ff4
alexey <skryabin.aleksey99@gmail.com>
2020-11-03 23:22:38 +0500

    Added a header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Header } from './components/header/Header';
 
 import './App.css';
 
-export const App = () => (
-    <div className="App">
-      Hello world!
-    </div>
+export const App = () => ( 
+  <div className="App">
+    <Header />
+
+    <Router>
+      <Switch>
+        <Route path="/" exact >
+          I am homepage
+        </Route>
+        <Route path="/chat" >
+          I am chatpage
+        </Route>
+        <Route path="/profile" >
+          I am prifile page
+        </Route>
+      </Switch>
+    </Router>
+  </div>
 );
